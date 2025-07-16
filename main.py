905e84c97483dbb9c9a95d34070a0fcd951e9285
Gabiimartire <gabbiimartire3@gmail.com>
2025-07-16 20:46:55 -0300

    Commit verificar si eiste el json de espacios de trabajo, antes de elegir su espacio

---
+++ main.py
@@ -0,0 +1,17 @@
+import json
+from pathlib import Path
+print("Buenasss")
+#Verificar si existe el JSON, para saber si en el primer uso lo crea 
+def verificar_json(ruta_del_json):
+    return ruta_del_json.exists()
+
+ruta = Path("espacios")/"espacios_de_trabajo.json"
+
+if not verificar_json(ruta):
+    espacios = [{}]
+    json_nuevo = "espacios_de_trabajo.json"
+    #CREAR EL JSON
+
+    print("Cree su primer espacio de trabajo")
+else:
+    print("Seleccione su espacio de trabajo en el que desea trabajar")
